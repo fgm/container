0e6194a57f3eaa0b26bf7505638a692c2f38366a
Frédéric G. MARAND <fgm@osinet.fr>
2025-04-07 22:48:10 +0200

    feat(list): first version of deque as fork from container/list (#45).
    - includes 100% test coverage and cmd/ demo with tests.

---
+++ list/example_test.go
@@ -0,0 +1,33 @@
+// Copyright 2013 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+// This file contains code from the go stdlib container/list package,
+// and is therefore dual licensed under the BSD 3 clause license, and the Apache 2.0 license.
+package list_test
+
+import (
+	"fmt"
+
+	"github.com/fgm/container/list"
+)
+
+func Example() {
+	// Create a new list and put some numbers in it.
+	l := list.New[int]()
+	e4 := l.PushBack(4)
+	e1 := l.PushFront(1)
+	l.InsertBefore(3, e4)
+	l.InsertAfter(2, e1)
+
+	// Iterate through list and print its contents.
+	for e := l.Front(); e != nil; e = e.Next() {
+		fmt.Println(e.Value)
+	}
+
+	// Output:
+	// 1
+	// 2
+	// 3
+	// 4
+}
